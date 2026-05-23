164c23f8e7b32a96f93755a4dbaf4c7b680b3eee
xsy <3027573019@qq.com>
2026-05-23 10:54:17 +0800

    20260523xsytest

--- task.js
+++ task.js
@@ -6,131 +6,75 @@
 TODO J1：
 创建一个变量 name
 保存你的名字（字符串）
-
-输出：
-你好，我是xxx
+输出：你好，我是xxx
 */
-
-
+let name = "xsy"
+console.log("你好，我是" + name);
 
 
 /*
 TODO J2：
-创建两个数字变量：
-
-a = 10
-b = 20
-
-输出它们的和
+创建两个数字变量 a=10 b=20，输出它们的和
 */
-
+let a = 10;
+let b = 20;
+console.log(a + b);
 
 
 /*
 TODO J3：
-创建一个布尔值变量：
-
-isStudent = true
-
-如果是 true 输出：
-我是学生
-
-否则输出：
-我不是学生
+布尔值变量isStudent，判断输出对应文字
 */
-
-
+let isStudent = true;
+if(isStudent === true){
+    console.log("我是学生");
+}else{
+    console.log("我不是学生");
+}
 
 
 /*
 TODO J4：
-创建函数：
-
-sayHello()
-
-调用后弹窗：
-
-欢迎学习 JavaScript
+创建sayHello函数，调用弹出提示
 */
-
-
-
+function sayHello(){
+    alert("欢迎学习 JavaScript");
+}
 
 
 /*
 TODO J5：
-补全 showMessage()
-
-点击按钮后：
-
-把页面中的：
-
-这里显示JS结果
-
-改成：
-
-按钮被点击了！
+点击修改文本内容
 */
 function showMessage() {
-    
+    document.querySelector("div").innerText = "按钮被点击了！";
 }
-
-
 
 
 /*
 TODO J6：
-创建数组：
-
-["HTML","CSS","JavaScript"]
-
-输出：
-
-1. 第一个元素
-2. 数组长度
+创建数组，输出首个元素和数组长度
 */
-
-
-
-
+const arr = ["HTML","CSS","JavaScript"]
+console.log(arr[0])
+console.log(arr.length);
 
 
 /*
 TODO J7：
-创建对象：
-
-student
-
-包含：
-
-name
-age
-score
-
-输出：
-
-student.name
+创建学生对象并输出姓名
 */
-
-
-
-
-
+const student ={
+    name:"小明",
+    age:18,
+    score:90
+}
+console.log(student.name);
 
 
 /*
-TODO J8（综合题）：
-补全函数：
-
-changeColor()
-
-点击按钮后：
-
-修改网页背景颜色
-
-提示：
-
-document.body.style.backgroundColor
+TODO J8：
+切换网页渐变背景
 */
 var colorIndex = 0;
 var bgColors = [
@@ -144,11 +88,8 @@ var bgColors = [
 
 function changeColor() {
     colorIndex = (colorIndex + 1) % bgColors.length;
-    
-
-
+    document.body.style.background = bgColors[colorIndex];
 }
-
 
 // 以上为js题目部分，下面为卡通白菜的代码，完成js题目后可以看看哦~
 
